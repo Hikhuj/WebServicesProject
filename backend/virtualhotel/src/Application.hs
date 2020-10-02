b0b2914bad1a89a70eb1490d77b26a63e21683d2
tr0b <trobin3000600@gmail.com>
2020-10-01 18:24:48 -0600

    Added Initial Handler imports

--- backend/virtualhotel/src/Application.hs
+++ backend/virtualhotel/src/Application.hs
@@ -40,6 +40,10 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 import Handler.Common
 import Handler.Home
 import Handler.Comment
+import Handler.User
+import Handler.Reservation
+import Handler.Room
+import Handler.Hotel
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
