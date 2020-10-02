8408164b12d27a53e16ed5df955d19ba2c7b479c
tr0b <trobin3000600@gmail.com>
2020-10-01 18:29:52 -0600

    Added Initial Login Route

--- backend/virtualhotel/src/Application.hs
+++ backend/virtualhotel/src/Application.hs
@@ -44,6 +44,7 @@ import Handler.User
 import Handler.Reservation
 import Handler.Room
 import Handler.Hotel
+import Handler.Login
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
