e065d3ee1fdb7920aa7dafe491e4ef847029c642
tr0b <trobin3000600@gmail.com>
2020-10-17 13:32:14 -0600

    Initial Transport Type Handler and Logic (also spec fil)

---
+++ backend/virtualhotel/src/Handler/TransportType.hs
@@ -0,0 +1,9 @@
+module Handler.TransportType where
+
+import Import
+
+getTransportTypeR :: TipoTransporteId -> Handler Html
+getTransportTypeR tipoTransporteId = error "Not yet implemented: getTransportTypeR"
+
+putTransportTypeR :: TipoTransporteId -> Handler Html
+putTransportTypeR tipoTransporteId = error "Not yet implemented: putTransportTypeR"
---
+++ backend/virtualhotel/test/Handler/TransportTypeSpec.hs
@@ -0,0 +1,14 @@
+module Handler.TransportTypeSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getTransportTypeR" $ do
+        error "Spec not implemented: getTransportTypeR"
+
+
+    describe "putTransportTypeR" $ do
+        error "Spec not implemented: putTransportTypeR"
+
