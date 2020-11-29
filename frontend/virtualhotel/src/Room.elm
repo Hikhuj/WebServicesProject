107a76aea60f40d1cd5549117024a440544981ee
tr0b <trobin0000@gmail.com>
2020-11-28 20:37:54 -0600

    FrontEndV1 - To add Initial JSON Deconding for Rooms Change: - Added Basic Room JSON decoding

--- frontend/virtualhotel/src/Room.elm
+++ frontend/virtualhotel/src/Room.elm
@@ -1,20 +1,33 @@
+{- Room View -}
+
+
 module Room exposing (main)
 
-import Array exposing (Array)
+-- Browser elements and sandbox
+-- Html
+-- Html Attributes
+-- Html Events
+-- Random library
+-- JSON Decoders (Official and Third Party, for large decodings)
 import Browser
-import Html exposing (Html, button, div, h1, h2, img, text, label, input)
-import Html.Attributes exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (class, classList, id, name, src, title, type_)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, bool, int, list, string, succeed)
+import Json.Decode.Pipeline exposing (optional, required)
 import Random
+
+-- Urlprefix (WIP. should be moved to .env)
+
 
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
-isEmpty : String -> Bool
-isEmpty str =
-    str == ""
+
+-- Selected Room record constant
 
 
 selectedRoom : { description : String, data : String }
@@ -22,114 +35,178 @@ selectedRoom =
     { description = "ClickedRoom", data = "1.jpeg" }
 
 
-type Msg 
-        = ClickedRoom String
-        | GotSelectedIndex Int
-        | ClickedSize ThumbnailSize
-        | ClickedSurpriseMe
 
+-- Msg type. What we expect to get from our REST API
+
+
+type Msg
+    = ClickedRoom String
+    | GotRandomRoom Room
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotRooms (Result Http.Error (List Room))
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ h1 [] [ text "Virtual Hotel" ]
-        , h2 [] [ text "Rooms" ]
-        , button [ onClick ClickedSurpriseMe ] [ text "Sorprendeme!" ]
-        , div [ id "thumbnails", class ( sizeToString model.chosenSize ) ] (List.map (viewThumbnail model.selectedUrl) model.rooms)
-        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
-        , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
-        ]
+    div [ class "content" ] <|
+        case model.status of
+            Loaded rooms selectedUrl ->
+                viewLoaded rooms selectedUrl model.chosenSize
+
+            Loading ->
+                []
+
+            Errored errorMessage ->
+                [ text ("Error: " ++ errorMessage) ]
+
+
+viewLoaded : List Room -> String -> ThumbnailSize -> List (Html Msg)
+viewLoaded rooms selectedUrl chosenSize =
+    [ h1 [] [ text "Room Grove" ]
+    , h2 [] [ text "Virtual Hotel" ]
+    , button [ onClick ClickedSurpriseMe ] [ text "Sorprendeme!" ]
+    , h3 [] [ text "Thumbnail Size: " ]
+    , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+    , div [ id "thumbnails", class (sizeToString chosenSize) ] (List.map (viewThumbnail selectedUrl) rooms)
+    , img [ class "large", src (urlPrefix ++ "large/" ++ selectedUrl) ] []
+    ]
 
 
 viewThumbnail : String -> Room -> Html Msg
 viewThumbnail selectedUrl thumb =
-    img [ src (urlPrefix ++ thumb.url), classList [ ( "selected", selectedUrl == thumb.url ) ], onClick (ClickedRoom thumb.url) ] []
+    img 
+     [ src (urlPrefix ++ thumb.url)
+      , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ "KB]")
+      , classList [ ( "selected", selectedUrl == thumb.url ) ]
+      , onClick (ClickedRoom thumb.url) ] []
+
 
 viewSizeChooser : ThumbnailSize -> Html Msg
-viewSizeChooser size = 
-        label []
+viewSizeChooser size =
+    label []
         [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
-        , text (sizeToString size) 
+        , text (sizeToString size)
         ]
+
 
 sizeToString : ThumbnailSize -> String
 sizeToString size =
-        case size of 
-                Small -> "small"
-                Medium -> "medium"
-                Large -> "large"
+    case size of
+        Small ->
+            "small"
 
-type ThumbnailSize 
-        = Small
-        | Medium
-        | Large
+        Medium ->
+            "medium"
+
+        Large ->
+            "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
 
 
 type alias Room =
-    { url : String }
+    { url : String
+    , size : Int
+    , title : String
+    }
+
+-- Room decoder fromJSON
+roomDecoder : Decoder Room
+roomDecoder = 
+        succeed Room
+         |> required "url" string
+         |> required "size" int
+         |> optional "title" string "(untitled)"
 
 
-type Status = 
-        Loaded ( List Room ) String
-        | Loading
-        | Error
-                
+type Status
+    = Loaded (List Room) String
+    | Loading
+    | Errored String
+
+
 type alias Model =
-    { rooms : List Room
-    , selectedUrl : String
+    { status : Status
     , chosenSize : ThumbnailSize
     }
 
 
 initialModel : Model
 initialModel =
-    { rooms =
-        [ { url = "1.jpeg" }
-        , { url = "2.jpeg" }
-        , { url = "3.jpeg" }
-        ]
-    , selectedUrl = "1.jpeg"
-    , chosenSize = Large
+    { status = Loading
+    , chosenSize = Medium
     }
 
-
-roomArray : Array Room
-roomArray =
-    Array.fromList initialModel.rooms
-
-
-getRoomUrl : Int -> String 
-getRoomUrl index = 
-        case Array.get index roomArray of 
-                Just room ->
-                        room.url
-                Nothing -> 
-                        ""
-
-randomRoomPicker : Random.Generator Int
-randomRoomPicker =
-        Random.int 0 ( Array.length roomArray - 1 )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotSelectedIndex index -> 
-            ( { model | selectedUrl = getRoomUrl index }, Cmd.none )
+        GotRandomRoom room ->
+            ( { model | status = selectUrl room.url model.status }, Cmd.none )
+
         ClickedRoom url ->
-            ( { model | selectedUrl = url }, Cmd.none)
+            ( { model | status = selectUrl url model.status }, Cmd.none )
 
-        ClickedSize size -> 
-            ( { model | chosenSize = size }, Cmd.none)
+        ClickedSize size ->
+            ( { model | chosenSize = size
+            }, Cmd.none )
+
+
         ClickedSurpriseMe ->
-            ( model, Random.generate GotSelectedIndex randomRoomPicker )
+            case model.status of
+                Loaded (firstRoom :: otherRooms) _ ->
+                    Random.uniform firstRoom otherRooms
+                        |> Random.generate GotRandomRoom
+                        |> Tuple.pair model
 
+                Loaded [] _ ->
+                    ( model, Cmd.none )
+
+                Loading ->
+                    ( model, Cmd.none )
+
+                Errored errorMessage ->
+                    ( model, Cmd.none )
+
+        GotRooms (Ok rooms) ->
+            case rooms of
+                first :: rest ->
+                    ( { model | status = Loaded rooms first.url }, Cmd.none )
+
+                [] ->
+                    ( { model | status = Errored "Ninguna Habitacion Encontrada" }, Cmd.none )
+
+        GotRooms (Err _) ->
+            ( model, Cmd.none )
+selectUrl : String -> Status -> Status
+selectUrl url status =
+    case status of
+        Loaded rooms _ ->
+            Loaded rooms url
+
+        Loading ->
+            status
+
+        Errored errorMessage ->
+            status
+
+{-- Initial Command for retrieving information from server, as an HTTP GET request  --}
+initialCmd : Cmd Msg
+initialCmd =
+        Http.get
+        { url = "http://elm-in-action.com/photos/list.json"
+        , expect = Http.expectJson GotRooms (list roomDecoder) }
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> ( initialModel , Cmd.none )
+        { init = \_ -> ( initialModel, initialCmd )
         , view = view
         , update = update
-        , subscriptions = \model -> Sub.none
+        , subscriptions = \_ -> Sub.none
         }
